e3853946c1262f131479a3ffe457076749a0e100
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2025-07-25 17:13:14 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+python3 -m venv venv
+source venv/bin/activate
+sudo apt install default-libmysqlclient-dev build-essential pkg-config
+cd frontend
+npm install 
+cd ../
+cd backend
+pip install -r requirements.txt
