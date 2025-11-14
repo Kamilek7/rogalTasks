6b2cfb3266c3c645a807750ed21d0f1131ce745c
kamilek7 <kamijar20031@gmail.com>
2025-11-14 22:59:07 +0100

    wsgi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { CookiesProvider} from 'react-cookie'
 import './index.css'
 import App from './App.jsx'
-import { createContext } from "react";
-export const backendLink = createContext();
+
 
 createRoot(document.getElementById('root')).render(
   <CookiesProvider>
